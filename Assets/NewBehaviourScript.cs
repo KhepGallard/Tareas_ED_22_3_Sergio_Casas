3df36fd5660765fe0e8863e747396ea45a35e502
KhepGallard <sergio.casasg@my.unitec.edu.mx>
2022-06-03 01:42:34 -0500

    se añadieron nuevos archivos

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TextMeshProUGUI texto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        texto.text="";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            texto.text="Bayonetta";
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            texto.text="Hades";
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            texto.text="NieR:Automata";
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            texto.text="PERSONA5";
+        }
+    }
+}
